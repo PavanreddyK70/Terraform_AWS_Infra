860e1ea081ef9035ba2bd02e0a89f95006ce46d1
PavanreddyK70 <pavanreddykodathala15@gmail.com>
2025-10-08 20:03:19 +0530

    backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "pavan-bucket-01"     # same bucket as above
+    bucket         = "pavanreddy-bucket-01"     # same bucket as above
     key            = "vpc/terraform.tfstate"        # folder/path for state file
     region         = "ap-south-1"                   # your AWS region
     dynamodb_table = "terraform-locks"              # same DynamoDB table name
